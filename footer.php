04666fb4204d3704a8a24f06e04815b3e0acf215
BigMouth <29755257+Utopianer@users.noreply.github.com>
2023-02-06 11:53:46 +0000

    add 10k 50k pizza to js

--- footer.php
+++ footer.php
@@ -33,7 +33,7 @@
         ).src = `./img/${selected.toUpperCase()}.png`;
       }
     </script>
-    <?php if ($page == 'account' || $page == 'shop' || $page == 'burn-oven' || $page == 'freemint' || $page == 'infinity' || $page == 'whitelist-lvl1' || $page == 'whitelist-lvl2' || $page == 'whitelist-lvl3' || $page == 'airdrop-pass' || $page == 'custom-pizza-V1' || $page == 'twitter-promo' || $page == '100k-pizza' || $page == 'burn-ovenv2'): ?>
+    <?php if ($page == 'account' || $page == 'shop' || $page == 'burn-oven' || $page == 'freemint' || $page == 'infinity' || $page == 'whitelist-lvl1' || $page == 'whitelist-lvl2' || $page == 'whitelist-lvl3' || $page == 'airdrop-pass' || $page == 'custom-pizza-V1' || $page == 'twitter-promo' || $page == '100k-pizza' || || $page == '50k-pizza' || $page == '10k-pizza' || $page == 'burn-ovenv2'): ?>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/web3/1.8.1/web3.min.js" integrity="sha512-vtUOC0YIaNm/UutU7yfnwqnF9LOYnXtpHe2kwi1nvJNloeGQuncNBiEKP/3Ww3D62USAhbXGsnYpAYoiDsa+wA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
       <script src="./js/shopabi.js"></script>
       <script src="./js/coinabi.js"></script>
