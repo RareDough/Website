95ef4c04ce46ce2cf3443464d0aaaec089e4e33a
BigMouth <29755257+Utopianer@users.noreply.github.com>
2023-04-11 21:36:27 +0000

    add 4 new pizzas

--- js/shop.js
+++ js/shop.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
    const category = shopSection.dataset.category;
    console.log(category);
-   for (a = 1; a < 25; a++) {
-     if (a === 10 || a === 11 || a === 12 || a === 13 || a === 14 || a === 15 || a === 16 || a === 17) {
+   for (a = 1; a < 29; a++) {
+     if (a === 10 || a === 11 || a === 12 || a === 13 || a === 14 || a === 15 || a === 16 || a === 17 || a === 25) {
          continue;
       }
       let itemIndex = a;
