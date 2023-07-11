6bfc9dd83f58c588bcf33900f6ca79549131dc32
Jdogg <jmcorris87@icloud.com>
2023-07-10 20:17:48 -0500

    When moderator approves a token, copy the image to the assets folder and generate the JSON file

--- inc/mod-tools.php
+++ inc/mod-tools.php
@@ -16,54 +16,9 @@ $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($user) {
 	$userLevel = $user['user_level'];
+	$userTwitter = $user['twitter_username'];
 
-	if ($userLevel == 0) {
-		// User is not a moderator
-		$stmt = $pdo->prepare('SELECT * FROM submissions WHERE token_id = ?');
-        $stmt->execute([$tokenID]);
-        $token = $stmt->fetch(PDO::FETCH_ASSOC);
-
-        if ($token) {
-        	// Get token details
-        	$tokenName = $token['name'];
-        	$tokenDesc = $token['description'];
-
-        	// Copy image and generate JSON
-			$path = $_SERVER['DOCUMENT_ROOT'].'/assets/' . $tokenID . '.json';
-		    $jsonData = [
-	            "name" => $tokenName,
-	            "symbol" => $tokenName,
-	            "description" => $tokenDesc,
-	            "image" => "https://raredough.com/assets/images/" . $tokenID . ".jpg",
-	            "external_link" => "https://raredough.com/custom-pizza?id=" . $tokenID,
-	            "attributes" => [
-	                [
-	                    "trait_type" => "Category",
-	                    "value" => "custom pizza"
-	                ],
-	                [
-	                    "trait_type" => "Price",
-	                    "value" => "100 BREAD"
-	                ],
-	                [
-	                    "trait_type" => "Utility",
-	                    "value" => "Honorary"
-	                ],
-	                [
-	                    "trait_type" => "URL",
-	                    "value" => ""
-	                ]
-	            ]
-	        ];
-		    // Convert JSON
-		    $jsonString = json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
-		    // Save JSON file
-		    $fp = fopen($path, 'w');
-		    fwrite($fp, $jsonString);
-		    fclose($fp);
-		    echo json_encode( array('token_activated' => true, 'message' => 'Token JSON and image have been generated') );
-		}
-	} else if ($userLevel == 10) {
+	if ($userLevel == 10) {
 		// User is a moderator
 		$stmt = $pdo->prepare('SELECT * FROM submissions WHERE token_id = ?');
         $stmt->execute([$tokenID]);
@@ -73,6 +28,47 @@ if ($user) {
             // Update the token database entry
             $stmt = $pdo->prepare('UPDATE submissions SET status = ? WHERE token_id = ?');
             $stmt->execute([ $status, $token['token_id'] ]);
+
+            // Generate JSON and transfer image file if enabling
+            if ($action == 'enable') {
+            	// Move image to assets folder
+			    $source = $_SERVER['DOCUMENT_ROOT'].explode('https://raredough.com', $token['image_path'])[1];
+			    $destination = $_SERVER['DOCUMENT_ROOT'].'/assets/custom/images/'.$tokenID.'.jpg';
+			    copy($source, $destination);
+
+            	$jsonPath = $_SERVER['DOCUMENT_ROOT'].'/assets/custom/' . $tokenID . '.json';
+			    $jsonData = [
+		            "name" => $tokenName,
+		            "symbol" => "",
+		            "description" => $tokenDesc,
+		            "image" => "https://raredough.com/assets/images/" . $tokenID . ".jpg",
+		            "external_link" => "https://raredough.com/custom-pizza?id=" . $tokenID,
+		            "attributes" => [
+		                [
+		                    "trait_type" => "Category",
+		                    "value" => "custom pizza"
+		                ],
+		                [
+		                    "trait_type" => "Price",
+		                    "value" => "100 BREAD"
+		                ],
+		                [
+		                    "trait_type" => "Utility",
+		                    "value" => "Honorary"
+		                ],
+		                [
+		                    "trait_type" => "URL",
+		                    "value" => $userTwitter
+		                ]
+		            ]
+		        ];
+			    // Convert JSON
+			    $jsonString = json_encode($jsonData, JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+			    // Save JSON file
+			    $fp = fopen($jsonPath, 'w');
+			    fwrite($fp, $jsonString);
+			    fclose($fp);
+            }
 
             echo json_encode( array('token_updated' => true, 'message' => 'Token #' . $tokenID . ' status has been changed to ' . $status) );
         } else {
