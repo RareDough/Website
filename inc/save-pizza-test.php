fbac52af8c2c3ad88eb81383e4e6a00dd43e3013
Jdogg <jmcorris87@icloud.com>
2023-09-02 11:36:47 -0500

    New $_POST test for metamask

--- inc/save-pizza-test.php
+++ inc/save-pizza-test.php
@@ -1,24 +1,10 @@
 <?php
-    ini_set('display_errors', 1);
-    ini_set('display_startup_errors', 1);
-    error_reporting(E_ALL);
-
     // Form variables
-    $userWallet = $_POST['userWallet'];
-    $imageData = $_POST['imgBase64'];
-    $id = $_POST['id'];
-    $supply = $_POST['supply'];
-    $name = $_POST['name'];
-    $description = $_POST['description'];
+    $userWallet = $_POST['wallet'];
 
     echo json_encode( 
         array(
-            'wallet' => $userWallet,
-            'image' => $imageData,
-            'id' => $id,
-            'supply' => $supply,
-            'name' => $name,
-            'desc' => $description
+            'wallet' => $userWallet
         )
     );
 ?>
